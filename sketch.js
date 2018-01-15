cae54c244892be22f1025dd889a1633cdadb7bcd
eliot_chill <eliotchill99@gmail.com>
2018-01-15 21:52:45 +0000

    Changed layout a lot

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// Shake the device to change the value.
+
+var value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function deviceShaken() {
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
+}
